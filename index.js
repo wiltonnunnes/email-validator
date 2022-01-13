af85ab3b6125b564b5fae2ba7fd4b0b21936e912
Wilton Nunes Gomes da Silva <will_nunnes@hotmail.com>
2022-01-13 20:15:56 -0300

    Editar função validate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
 }
 
 function validate(email) {
-  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value)) {
+  if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)) {
     return (true)
   }
   return (false)
